8ca95f64c621782d79d6fdb3419e1bd4d38d204f
Nitesh Yadav <nteshxx@gmail.com>
2022-11-21 15:04:14 +0530

    solved tower of hanoi

---
+++ Introductory Problems/TowerofHanoi.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TowerofHanoi {
+
+    private static void towerOfHanoi(int n, char source, char middle, char destination) {
+        // base case
+        if (n == 1) {
+            System.out.println(source + " " + destination);
+            return;
+        }
+        towerOfHanoi(n - 1, source, destination, middle);
+        System.out.println(source + " " + destination);
+        towerOfHanoi(n - 1, middle, source, destination);
+    }
+
+    public static void main(String[] args) {
+        
+        // Initializing scanner for taking inputs
+        Scanner sc = new Scanner(System.in);
+        // no. of disks
+        int n = sc.nextInt();
+        // closing input stream
+        sc.close();
+
+        // tower of hanoi has 2^n - 1 steps
+        System.out.println((1<<n)-1);
+        // solve
+        towerOfHanoi(n, '1', '2', '3');
+    }
+}
