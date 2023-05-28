fdadb53571795cc343e2a36fbcb9e69e3fd21272
Nitesh Yadav <nteshxx@gmail.com>
2023-05-28 17:57:41 +0530

    weird algorith java

---
+++ Introductory Problems/java/WeirdAlgorithm.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+
+    public static void main(String[] args) {    
+        // Initializing scanner for taking inputs
+        Scanner sc = new Scanner(System.in);
+        // integer n
+        long n = sc.nextLong();
+
+        // print n
+        System.out.print(n);
+
+        // weird algorithm
+        while (n != 1) {
+            n = (n % 2 == 0) ? n / 2 : (n * 3) + 1;
+            System.out.print(" " + n);
+        }
+    }
+}
