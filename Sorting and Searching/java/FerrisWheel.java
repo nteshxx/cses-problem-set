240034287af304b5b349fe336f77ed73424a4aca
Nitesh Yadav <nteshxx@gmail.com>
2023-08-13 11:56:18 +0530

    optimized Ferris Wheel

--- Sorting and Searching/java/FerrisWheel.java
+++ Sorting and Searching/java/FerrisWheel.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class FerrisWheel {
 
-    private static int allocateGondolas(int childrens, long maxWeight, long[] weights) {
+    private static void allocateGondolas(int childrens, long maxWeight, Integer[] weights) {
         Arrays.sort(weights);
 
         int count = 0;
@@ -25,139 +25,35 @@ public class FerrisWheel {
             }
         }
 
-        return count;
+        // print the solution
+        System.out.println(count);
+
+        return;
     }
     
     public static void main(String[] args) throws IOException {
         // reader
-        Reader read = new Reader();
+        Scanner read = new Scanner(System.in);
         
         int childrens = read.nextInt();
-        long maxWeight = read.nextLong();
+        long maxWeight = read.nextInt();
+        read.nextLine();
  
-        long[] weights = new long[childrens];
-
+        // Reading weights of children (this optimization is necessary to pass all test cases)
+		String[] weightsStr = read.nextLine().split(" ");
+		
+        // converting the weights to Long (part of optimization)
+        Integer[] weights = new Integer[childrens];
         for (int i = 0; i < childrens; i++) {
-            weights[i] = read.nextLong();
-        }
+			weights[i] = Integer.parseInt(weightsStr[i]);
+		}
  
-        // solve and print the solution
-        System.out.print(allocateGondolas(childrens, maxWeight, weights));
+        // solve
+        allocateGondolas(childrens, maxWeight, weights);
+
+        // close stream
+        read.close();
  
         return;
-
-    }
-
-    public static class Reader {
-        final private int BUFFER_SIZE = 1 << 16;
-        private DataInputStream din;
-        private byte[] buffer;
-        private int bufferPointer, bytesRead;
-
-        public Reader() {
-            din = new DataInputStream(System.in);
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public Reader(String file_name) throws IOException {
-            din = new DataInputStream(
-                    new FileInputStream(file_name));
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public String readLine() throws IOException {
-            byte[] buf = new byte[64]; // line length
-            int cnt = 0, c;
-            while ((c = read()) != -1) {
-                if (c == '\n') {
-                    if (cnt != 0) {
-                        break;
-                    } else {
-                        continue;
-                    }
-                }
-                buf[cnt++] = (byte) c;
-            }
-            return new String(buf, 0, cnt);
-        }
-
-        public int nextInt() throws IOException {
-            int ret = 0;
-            byte c = read();
-            while (c <= ' ') {
-                c = read();
-            }
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        public long nextLong() throws IOException {
-            long ret = 0;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        public double nextDouble() throws IOException {
-            double ret = 0, div = 1;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            if (c == '.') {
-                while ((c = read()) >= '0' && c <= '9') {
-                    ret += (c - '0') / (div *= 10);
-                }
-            }
-
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        private void fillBuffer() throws IOException {
-            bytesRead = din.read(buffer, bufferPointer = 0,
-                    BUFFER_SIZE);
-            if (bytesRead == -1)
-                buffer[0] = -1;
-        }
-
-        private byte read() throws IOException {
-            if (bufferPointer == bytesRead)
-                fillBuffer();
-            return buffer[bufferPointer++];
-        }
-
-        public void close() throws IOException {
-            if (din == null)
-                return;
-            din.close();
-        }
     }
 }
